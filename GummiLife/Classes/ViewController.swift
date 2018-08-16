76a90968fd42b8167863ebe00a6355b5acb5585a
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2018-08-16 19:12:28 +0900

    add Libraries

---
+++ GummiLife/Classes/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GummiLife
+//
+//  Created by Kenzo on 3/4/18.
+//  Copyright © 2018 gummiapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "gummi"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'GummiLife' do
+  use_frameworks!
+  
+  pod 'Firebase/Core'
+  pod 'Firebase/Firestore'
+  pod 'R.swift'
+end
+
+target 'GummiLife copy' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GummiLife copy
+
+end
